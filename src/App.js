ba27c75f5364995d25944706f334a0963a7410c9
simeon.ieroteev <simeon.ieroteev@sofyma.com>
2020-05-27 16:28:31 +0300

    Deployed to netlify.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Routing from './components/core/Routing/Routing';
 
 function App() {
-  return (
+    return(
     <div className="App">
-      <Routing />
+        <Routing />
     </div>
-  );
+    );
 };
 
 export default App;
